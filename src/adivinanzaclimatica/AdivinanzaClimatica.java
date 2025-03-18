e460fcada7b066ef58ed75843a949b1bc4ff2cb9
carrotlover25 <fang@uninorte.edu.co>
2025-03-18 16:21:38 -0500

    metodo de ruta agregado, asimismo las variables para la temperatura secreta

--- src/adivinanzaclimatica/AdivinanzaClimatica.java
+++ src/adivinanzaclimatica/AdivinanzaClimatica.java
@@ -4,7 +4,9 @@
  */
 package adivinanzaclimatica;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -17,15 +19,35 @@ public class AdivinanzaClimatica {
      */
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         Arbol arbol = new Arbol();
+        ArrayList<Integer> temperatura = new ArrayList<>();
         int tries = 0;
+        int sw = 0;
+        
         System.out.println("¡Adivinanza Climatica!");
         System.out.println("¡Adivine la temperatura promedio global en 4 intentos!");
-        for (int i = 0; i < 12; i++) {
+        
+        for (int i = 0; i < 31; i++) {
+            int a = random.nextInt(-10, 50);
+             temperatura.add(a);       
+        }
+        
+        for (Integer t : temperatura) {
+            arbol.insertarNodo(t);
+        }
+        
+        int i = random.nextInt(temperatura.size());
+        int temp_secreta = temperatura.get(i);
+                
+                /* for (int i = 0; i < 20; i++) {
             int a = random.nextInt(-10, 50);
             arbol.insertarNodo(a);
-        }
+        } */
         arbol.imprimir(arbol.raiz, 0);
+        
+        
+        
     }
     
 }
--- src/adivinanzaclimatica/Arbol.java
+++ src/adivinanzaclimatica/Arbol.java
@@ -16,23 +16,26 @@ public class Arbol {
         if (this.raiz == null){
             this.raiz = nuevonodo;
         } else {
-          insertarRecursivo(this.raiz, nuevonodo);  
+          insertarRecursivo(this.raiz, nuevonodo, 1);  
         }
     }
     
-    public void insertarRecursivo(Nodo nodo, Nodo nuevonodo){
+    public void insertarRecursivo(Nodo nodo, Nodo nuevonodo, int nivel){
+        if (nivel >= 5){
+            return;
+        }
         if (nuevonodo.dato < nodo.dato){
             if (nodo.izquierdo == null){
                 nodo.izquierdo = nuevonodo;
             } else {
-                insertarRecursivo(nodo.izquierdo, nuevonodo);
+                insertarRecursivo(nodo.izquierdo, nuevonodo, nivel++);
             }
         } else {
             if (nuevonodo.dato > nodo.dato){
                 if (nodo.derecho == null){
                     nodo.derecho = nuevonodo;
                 } else {
-                    insertarRecursivo(nodo.derecho, nuevonodo);
+                    insertarRecursivo(nodo.derecho, nuevonodo, nivel++);
                 }
             }
         }
@@ -61,5 +64,34 @@ public class Arbol {
             imprimir (n1.derecho, nivel+1 );
            
         }
+    }
+    
+    public int altura (Nodo nodo){
+          if (nodo == null){
+              return 0;
+          } return Math.max(altura(nodo.izquierdo), altura(nodo.derecho)) + 1;
+      }
+    
+    public boolean ruta (int elemento, int secreto){
+        Nodo current = raiz;
+        int nivel = 1;
+        System.out.print("Ruta: ");
+        while (current != null){
+            System.out.println(current.dato + " -> ");
+            
+            if (elemento == current.dato){
+                System.out.println("Esta no es la temperatura correcta. Sigue Intentando");
+                return false;
+            } else if (elemento == secreto){
+                System.out.println("¡Has acertado, felicitaciones!");
+                return true;
+            } else if (elemento < current.dato ){
+                current = current.izquierdo;
+            } else {
+                current = current.derecho;
+            }
+            nivel++;
+        }
+        return false;
     }
 }
